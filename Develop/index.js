7736c25ca84429f38dced4576083ace99d766311
kofithebass <kofi@kofihayford.com>
2021-04-04 16:41:06 -0400

    Update index.js
    created another get for the notes page because it was going to a blank page. Added pseudo code. added the app.use to fix the "Cannot load message" when I first loaded the app in the browser.

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,26 @@
+//declare the appropriate and needed variables. 
 const express = require("express")
+const path = require("path")
 const app = express()
 
+//define the path that should be used to get access to the required files. In this case, the important files are in the public folder. 
+app.use("/", express.static(path.join(__dirname, "public")))
+console.log(__dirname);
 
+//retrieve the informaiton/file and send it or show it
+app.get("/", (req, res) => {
+    res.sendFile("index.html")
+})
+app.get("/notes", (req, res) => {
+    res.sendFile("notes.html")
+})
+
+
+//Get the app to listen to the appropriate host. 
 app.listen(7000, () => {
+    //use console log to ensure that it is working. 
     console.log("listening to server")
 });
-
 
 
 
